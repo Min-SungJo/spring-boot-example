1e07eb1da0a69f132ece80020211241cce3a7ef9
Min-SungJo <123or@naver.com>
2023-02-19 02:27:13 +0900

    jpa 활용 crud

--- src/main/java/com/sj/Main.java
+++ src/main/java/com/sj/Main.java
@@ -2,20 +2,66 @@ package com.sj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 //@ComponentScan(basePackages = "com.sj")
 //@EnableAutoConfiguration
 //@Configuration
 @SpringBootApplication
 @RestController
+@RequestMapping("api/vi/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args){
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
+
+    }
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody NewCustomerRequest request){
+        Optional<Customer> customer = customerRepository.findById(id);
+        customer.ifPresent(selectCustomer->{
+            selectCustomer.setName(request.name());
+            selectCustomer.setEmail(request.email());
+            selectCustomer.setAge(request.age());
+
+            customerRepository.save(selectCustomer);
+        });
     }
 
     @GetMapping("/greet")
